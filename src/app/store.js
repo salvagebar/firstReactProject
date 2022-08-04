dccffd98b8e83be4b7be19f92b6c8bb1e07a047b
salvagebar <salvagebar@gmail.com>
2022-08-03 21:36:53 -0400

    Campsites useSelector

--- src/app/store.js
+++ src/app/store.js
@@ -1,5 +1,4 @@
 import { configureStore } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
 import { campsitesReducer } from '../features/campsites/campsitesSlice';
 
 export const store = configureStore({

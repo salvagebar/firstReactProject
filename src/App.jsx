b2d91d6d3577d4a6d8b38677cda4d7b76f4d7f87
salvagebar <salvagebar@gmail.com>
2022-08-13 11:39:06 -0400

    Async Campsites

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,7 @@
 import { Routes, Route } from 'react-router-dom';
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { fetchCampsites } from './features/campsites/campsitesSlice';
 import HomePage from './pages/HomePage';
 import ContactPage from './pages/ContactPage';
 import CampsitesDirectoryPage from './pages/CampsitesDirectoryPage';
@@ -8,8 +11,11 @@ import Header from './components/Header';
 import Footer from './components/Footer';
 import './App.css';
 
-
 function App() {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(fetchCampsites());
+  }, [dispatch]);
   return (
     <div className="App">
       <Header />

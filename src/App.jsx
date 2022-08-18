c44af73eed707f25ec3f0ddbc7e504c97573884d
salvagebar <salvagebar@gmail.com>
2022-08-18 07:56:23 -0400

    Week 5 Workshop Assignment

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { useDispatch } from 'react-redux';
 import { fetchCampsites } from './features/campsites/campsitesSlice';
 import { fetchPartners } from './features/partners/partnersSlice';
 import { fetchPromotions } from './features/promotions/promotionsSlice';
+import { fetchComments } from './features/comments/commentsSlice';
 import HomePage from './pages/HomePage';
 import ContactPage from './pages/ContactPage';
 import CampsitesDirectoryPage from './pages/CampsitesDirectoryPage';
@@ -19,6 +20,7 @@ function App() {
     dispatch(fetchCampsites());
     dispatch(fetchPartners());
     dispatch(fetchPromotions());
+    dispatch(fetchComments());
   }, [dispatch]);
 
   return (
--- src/features/comments/CommentsList.jsx
+++ src/features/comments/CommentsList.jsx
@@ -1,11 +1,31 @@
 import { useSelector } from 'react-redux';
-import { Col } from 'reactstrap';
+import { Col, Row } from 'reactstrap';
 import Comment from './Comment';
 import { selectCommentsByCampsiteId } from './commentsSlice';
 import CommentForm from './CommentForm';
+import Error from '../../components/Error';
+import Loading from '../../components/Loading';
 
 const CommentsList = ({ campsiteId }) => {
     const comments = useSelector(selectCommentsByCampsiteId(campsiteId));
+    const isLoading = useSelector((state) => state.comments.isLoading);
+    const errMsg = useSelector((state) => state.comments.errMsg);
+
+    if (isLoading) {
+        return (
+            <Row>
+                <Loading />
+            </Row>
+        );
+    }
+
+    if (errMsg) {
+        return (
+            <Row>
+                <Error errMsg={errMsg} />
+            </Row>
+        );
+    }
 
     if (comments && comments.length > 0) {
         return (
@@ -17,7 +37,7 @@ const CommentsList = ({ campsiteId }) => {
                 <CommentForm campsiteId={campsiteId}></CommentForm>
             </Col>
         );
-    } 
+    }
     return (
         <Col md='5' className='m-1'>There are no comments for this campsite yet.</Col>
     );
